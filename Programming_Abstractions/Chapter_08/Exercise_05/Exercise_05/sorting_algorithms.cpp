8d45ab24efb6cd78c508ec5c1a6d40ad7c9b8292
daniellozuz <daniel.zuziak@gmail.com>
2018-02-09 18:26:12 +0100

    Freaking bug.

--- Programming_Abstractions/Chapter_08/Exercise_05/Exercise_05/sorting_algorithms.cpp
+++ Programming_Abstractions/Chapter_08/Exercise_05/Exercise_05/sorting_algorithms.cpp
@@ -3,40 +3,41 @@
 
 using namespace std;
 
-void show(vector<int> &vec);
-void selection_sort(vector<int> &vec);
-void merge_sort(vector<int> &vec);
-void merge(vector<int> &vec, vector<int> &v1, vector<int> &v2);
-void quick_sort(vector<int> &vec);
-void quick_sort(vector<int> &vec, int start, int finish);
-int partition(vector<int> &vec, int start, int finish);
-void swap(vector<int> &vec, int i, int j);
+void show(int vec[], int size);
+void selection_sort(int vec[], int size);
+int *merge_sort(int vec[], int size);
+int *merge(int v1[], int size1, int v2[], int size2);
+//void quick_sort(int vec[], int size);
+//void quick_sort(int vec[], int start, int finish);
+//int partition(int vec[], int start, int finish);
+void swap(int vec[], int i, int j);
+int *append(int vec[], int size, int value);
 
 int main(void) {
-	vector<int> vec1 = { 1, 4, 5, 2, 6, 1, 8, 0 };
-	show(vec1);
-	selection_sort(vec1);
-	show(vec1);
+	int vec1[8] = { 1, 4, 5, 2, 6, 1, 8, 0 };
+	show(vec1, 8);
+	selection_sort(vec1, 8);
+	show(vec1, 8);
 
-	vector<int> vec2 = { 1, 4, 5, 2, 6, 1, 8, 0 };
-	show(vec2);
-	merge_sort(vec2);
-	show(vec2);
-
-	vector<int> vec3 = { 1, 4, 5, 2, 6, 1, 8, 0 };
-	show(vec3);
-	quick_sort(vec3);
-	show(vec3);
-
+	int vec2[8] = { 1, 4, 5, 2, 6, 1, 8, 0 };
+	show(vec2, 8);
+	int *vec3;
+	vec3 = merge_sort(vec2, 8);
+	show(vec3, 8);
+	/*
+	int vec4[8] = { 1, 4, 5, 2, 6, 1, 8, 0 };
+	show(vec4, 8);
+	quick_sort(vec4, 8);
+	show(vec4, 8);
+	*/
 	cin.get();
 	return 0;
 }
 
-void selection_sort(vector<int> &vec) {
-	int n = vec.size();
-	for (int lh = 0; lh < n; lh++) {
+void selection_sort(int vec[], int size) {
+	for (int lh = 0; lh < size; lh++) {
 		int rh = lh;
-		for (int i = lh + 1; i < n; i++) {
+		for (int i = lh + 1; i < size; i++) {
 			if (vec[i] < vec[rh])
 				rh = i;
 		}
@@ -44,54 +45,71 @@ void selection_sort(vector<int> &vec) {
 	}
 }
 
-void merge_sort(vector<int> &vec) {
-	int n = vec.size();
-	if (n <= 1)
-		return;
-	vector<int> v1;
-	vector<int> v2;
-	for (int i = 0; i < n; i++) {
-		if (i < n / 2)
-			v1.push_back(vec[i]);
+int *merge_sort(int vec[], int size) {
+	if (size <= 1)
+		return vec;
+	int *v1 = new int[0];
+	int v1_size = 0;
+	int *v2 = new int[0];
+	int v2_size = 0;
+	for (int i = 0; i < size; i++) {
+		if (i < size / 2)
+			v1 = append(v1, v1_size++, vec[i]);
 		else
-			v2.push_back(vec[i]);
+			v2 = append(v2, v2_size++, vec[i]);
 	}
-	merge_sort(v1);
-	merge_sort(v2);
-	vec.clear();
-	merge(vec, v1, v2);
+	v1 = merge_sort(v1, v1_size);
+	v2 = merge_sort(v2, v2_size);
+	vec = merge(v1, v1_size, v2, v2_size);
+	return vec;
 }
 
-void merge(vector<int> &vec, vector<int> &v1, vector<int> &v2) {
-	int n1 = v1.size();
-	int n2 = v2.size();
+int *append(int vec[], int size, int value) {
+	int *new_vec = new int[size + 1];
+	for (int i = 0; i < size; i++)
+		new_vec[i] = vec[i];
+	new_vec[size] = value;
+	return new_vec;
+}
+
+int *merge(int v1[], int size1, int v2[], int size2) {
+	int *new_vec = new int[0];
 	int p1 = 0;
 	int p2 = 0;
-	while (p1 < n1 && p2 < n2) {
-		if (v1[p1] < v2[p2])
-			vec.push_back(v1[p1++]);
-		else
-			vec.push_back(v2[p2++]);
+	while (p1 < size1 && p2 < size2) {
+		if (v1[p1] < v2[p2]) {
+			new_vec = append(new_vec, p1 + p2, v1[p1]);
+			p1++;
+		}
+		else {
+			new_vec = append(new_vec, p1 + p2, v2[p2]);
+			p2++;
+		}
 	}
-	while (p1 < n1)
-		vec.push_back(v1[p1++]);
-	while (p2 < n2)
-		vec.push_back(v2[p2++]);
+	while (p1 < size1) {
+		new_vec = append(new_vec, p1 + p2, v1[p1]);
+		p1++;
+	}
+	while (p2 < size2) {
+		new_vec = append(new_vec, p1 + p2, v2[p2]);
+		p2++;
+	}
+	return new_vec;
 }
 
-void quick_sort(vector<int> &vec) {
+/*void quick_sort(vector<int> &vec) {
 	quick_sort(vec, 0, vec.size() - 1);
-}
+}*/
 
-void quick_sort(vector<int> &vec, int start, int finish) {
+/*void quick_sort(vector<int> &vec, int start, int finish) {
 	if (start >= finish)
 		return;
 	int boundary = partition(vec, start, finish);
 	quick_sort(vec, start, boundary - 1);
 	quick_sort(vec, boundary + 1, finish);
-}
+}*/
 
-int partition(vector<int> &vec, int start, int finish) {
+/*int partition(vector<int> &vec, int start, int finish) {
 	int pivot = vec[start];
 	int lh = start + 1;
 	int rh = finish;
@@ -109,16 +127,16 @@ int partition(vector<int> &vec, int start, int finish) {
 	vec[start] = vec[lh];
 	vec[lh] = pivot;
 	return lh;
-}
+}*/
 
-void show(vector<int> &vec) {
+void show(int vec[], int size) {
 	cout << "[";
-	for (int number : vec)
-		cout << number << ", ";
-	cout << "]" << endl;
+	for (int i = 0; i < size; i++)
+		cout << vec[i] << ", ";
+	cout << "]" << endl << endl;
 }
 
-void swap(vector<int> &vec, int i, int j) {
+void swap(int vec[], int i, int j) {
 	int temp = vec[i];
 	vec[i] = vec[j];
 	vec[j] = temp;
