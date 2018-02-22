2c6534c9fad90cdcf147bf7ed59224ab895251b2
daniellozuz <daniel.zuziak@gmail.com>
2018-02-22 13:35:43 +0100

    Implemented negative repetitions and backspace.

---
+++ Programming_Abstractions/Chapter_10/Exercise_04/Exercise_04/EditorBuffer.cpp
@@ -0,0 +1,76 @@
+#include "EditorBuffer.h"
+#include <iostream>
+
+using namespace std;
+
+const int INITIAL_CAPACITY = 6;
+
+EditorBuffer::EditorBuffer() {
+	capacity = INITIAL_CAPACITY;
+	buffer = new char[capacity];
+	cursor.before = 0;
+	cursor.after = capacity;
+}
+
+EditorBuffer::~EditorBuffer() {
+	delete[] buffer;
+}
+
+void EditorBuffer::move_cursor_forward() {
+	if (cursor.after != capacity)
+		buffer[cursor.before++] = buffer[cursor.after++];
+}
+
+void EditorBuffer::move_cursor_backward() {
+	if (cursor.before != 0)
+		buffer[--cursor.after] = buffer[--cursor.before];
+}
+
+void EditorBuffer::move_cursor_to_start() {
+	while (cursor.before != 0)
+		move_cursor_backward();
+}
+
+void EditorBuffer::move_cursor_to_end() {
+	while (cursor.after != capacity)
+		move_cursor_forward();
+}
+
+void EditorBuffer::insert_character(char ch) {
+	if (cursor.before == cursor.after)
+		expand_capacity();
+	buffer[cursor.before++] = ch;
+}
+
+void EditorBuffer::delete_character() {
+	if (cursor.after != capacity)
+		cursor.after++;
+}
+
+void EditorBuffer::backspace_character() {
+	if (cursor.before != 0)
+		cursor.before--;
+}
+
+void EditorBuffer::display() {
+	for (int i = 0; i < cursor.before; i++)
+		cout << ' ' << buffer[i];
+	for (int i = cursor.after; i < capacity; i++)
+		cout << ' ' << buffer[i];
+	cout << endl;
+	for (int i = 0; i < cursor.before; i++)
+		cout << "  ";
+	cout << '^' << endl;
+}
+
+void EditorBuffer::expand_capacity() {
+	char *old_buffer = buffer;
+	buffer = new char[2 * capacity];
+	for (int i = 0; i < cursor.before; i++)
+		buffer[i] = old_buffer[i];
+	for (int i = cursor.after; i < capacity; i++)
+		buffer[capacity + i] = old_buffer[i];
+	cursor.after += capacity;
+	capacity *= 2;
+	delete[] old_buffer;
+}
---
+++ Programming_Abstractions/Chapter_10/Exercise_04/Exercise_04/EditorBuffer.h
@@ -0,0 +1,31 @@
+#ifndef _editorbuffer_h
+#define _editorbuffer_h
+
+class EditorBuffer
+{
+public:
+	EditorBuffer();
+	~EditorBuffer();
+
+	void move_cursor_forward();
+	void move_cursor_backward();
+	void move_cursor_to_start();
+	void move_cursor_to_end();
+	void insert_character(char ch);
+	void delete_character();
+	void backspace_character();
+	void display();
+
+private:
+	struct cursor_t {
+		int before, after;
+	};
+
+	char *buffer;
+	int capacity;
+	cursor_t cursor;
+
+	void expand_capacity();
+};
+
+#endif
